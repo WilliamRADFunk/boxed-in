e23461a99ca8aee7cfb09499a84ea17e9fe9cade
Bioninja01 <vinsonjames02@gmail.com>
2016-03-12 18:08:27 -0500

    BigBrother Placement
    fix BB placement on the board

--- Boxed-In/Assets/Scripts/GameBoardGenerator.cs
+++ Boxed-In/Assets/Scripts/GameBoardGenerator.cs
@@ -13,7 +13,6 @@ public class GameBoardGenerator : MonoBehaviour {
 
     public void Awake() {
         populateGraph_Nodes();
-       // populateGraph_BigBrothers();
     }
 
     public List<Node> getNodes() { return nodes; }
@@ -22,41 +21,23 @@ public class GameBoardGenerator : MonoBehaviour {
     private void populateGraph_Nodes() {
         for (int i = (int)-(boardSize / 2); i < (int)(boardSize / 2); i++) {
             for (int j = (int)-(boardSize / 2); j < (int)(boardSize / 2); j++) {
-                int counter = 1;
+              
                 for (int k = (int)-(boardSize / 2); k < (int)(boardSize / 2); k++) {
                     GameObject go = (GameObject)Instantiate(node, new Vector3(i * space, j * space, k * space), Quaternion.identity);
                     go.transform.SetParent(this.transform);
                     go.name = "node";
                     Node n = go.GetComponent<Node>();
                     nodes.Add(n);
-                    if(k> (int)-(boardSize / 2) && counter % 4 == 0) {
-                        counter = 1;
-                        GameObject bb = (GameObject)Instantiate(test, new Vector3(i, j, k), Quaternion.identity);
+                    if(i == (int)(boardSize / 2) -1 || j == (int)(boardSize / 2) - 1) { }
+                    else {
+                        GameObject bb = (GameObject)Instantiate(test, new Vector3(i * space + space / 2, j * space + space / 2, k * space + space / 2), Quaternion.identity);
                         bb.transform.SetParent(this.transform);
                         bb.name = "test";
                     }
-                    else {
-                        counter++;
-                    }
+                   
                 }
             }
         }
     }
 
-    /*
-    private void populateGraph_BigBrothers() {
-        Debug.Log((int)-(boardSize - 1) / 2);
-        for (int i = (int)-(boardSize-1)/2; i < (int)(boardSize - 1) / 2; i++) {
-            for (int j = (int)-(boardSize - 1) / 2; j < (int)(boardSize - 1) / 2; j++) {
-                for (int k = (int)-(boardSize - 1) / 2; k < (int)(boardSize - 1) / 2; k++) {
-                    GameObject go = (GameObject)Instantiate(test, new Vector3(i, j , k ), Quaternion.identity);
-                    go.transform.SetParent(this.transform);
-                    go.name = "test";
-                    Node n = go.GetComponent<Node>();
-                    nodes.Add(n);
-                }
-            }
-        }
-    }
-    */
 }
