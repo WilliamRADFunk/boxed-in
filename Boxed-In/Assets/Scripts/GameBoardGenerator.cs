f80f4653522e296d411cf1fcb23c05ae81126086
Jorge Rodriguez <jito.rodriguez@outlook.com>
2016-03-13 19:12:41 -0400

    Fixes GameBoardGenerator to spawn children around center of parent

--- Boxed-In/Assets/Scripts/GameBoardGenerator.cs
+++ Boxed-In/Assets/Scripts/GameBoardGenerator.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class GameBoardGenerator : MonoBehaviour {
+public class GameBoardGenerator : MonoBehaviour
+{
 
     public int boardSize = 4;
     public int space = 2;
+    private float skew;
     public GameObject node;
     public GameObject test;
 
     private List<BigBrother> bigBrothers = new List<BigBrother>();
     private List<Node> nodes = new List<Node>();
 
-    public void Awake() {
+    public void Awake()
+    {
+        if (boardSize % 2 == 0)
+        {
+            skew = space / 2;
+        }
+        else
+        {
+            skew = 0f;
+        }
         populateGraph_Nodes();
         populateGraph_BigBrother();
     }
@@ -19,11 +30,17 @@ public class GameBoardGenerator : MonoBehaviour {
     public List<Node> getNodes() { return nodes; }
     public List<BigBrother> getBigBrothers() { return bigBrothers; }
 
-    private void populateGraph_Nodes() {
-        for (int i = (int)-(boardSize / 2); i < (int)(boardSize / 2); i++) {
-            for (int j = (int)-(boardSize / 2); j < (int)(boardSize / 2); j++) { 
-                for (int k = (int)-(boardSize / 2); k < (int)(boardSize / 2); k++) {
-                    GameObject go = (GameObject)Instantiate(node, new Vector3(i * space, j * space, k * space), Quaternion.identity);
+    private void populateGraph_Nodes()
+    {
+
+        for (int i = (int)-(boardSize / 2); i < (int)(boardSize / 2); i++)
+        {
+            for (int j = (int)-(boardSize / 2); j < (int)(boardSize / 2); j++)
+            {
+                for (int k = (int)-(boardSize / 2); k < (int)(boardSize / 2); k++)
+                {
+                    GameObject go = (GameObject)Instantiate(node, new Vector3(i * space + skew, j * space + skew, k * space + skew), Quaternion.identity);
+                    Debug.Log(i + " " + j + " " + k);
                     go.transform.SetParent(this.transform);
                     go.name = "node";
                     Node n = go.GetComponent<Node>();
@@ -34,15 +51,21 @@ public class GameBoardGenerator : MonoBehaviour {
     }
 
 
-    private void populateGraph_BigBrother() {
-        for (int i = (int)-(boardSize / 2); i < (int)(boardSize / 2); i++) {
-            for (int j = (int)-(boardSize / 2); j < (int)(boardSize / 2); j++) {
-                for (int k = (int)-(boardSize / 2); k < (int)(boardSize / 2); k++) {
+    private void populateGraph_BigBrother()
+    {
+        for (int i = (int)-(boardSize / 2); i < (int)(boardSize / 2); i++)
+        {
+            for (int j = (int)-(boardSize / 2); j < (int)(boardSize / 2); j++)
+            {
+                for (int k = (int)-(boardSize / 2); k < (int)(boardSize / 2); k++)
+                {
                     if (i == (int)(boardSize / 2) - 1 ||
                         j == (int)(boardSize / 2) - 1 ||
-                        k == (int)(boardSize / 2) - 1) { }
-                    else {
-                        GameObject bb = (GameObject)Instantiate(test, new Vector3(i * space + space / 2, j * space + space / 2, k * space + space / 2), Quaternion.identity);
+                        k == (int)(boardSize / 2) - 1)
+                    { }
+                    else
+                    {
+                        GameObject bb = (GameObject)Instantiate(test, new Vector3(i * space + (2 * skew), j * space + (2 * skew), k * space + (2 * skew)), Quaternion.identity);
                         bb.transform.SetParent(this.transform);
                         bb.name = "BigBrother";
                     }
